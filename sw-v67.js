088f0847b0d95469da6904de525754b7b4e38de4
andreae6023re <and.estebaranz@gmail.com>
2026-09-11 12:10:01 +0200

    Add files via upload

---
+++ sw-v67.js
@@ -0,0 +1,11 @@
+const CACHE='mis-cosas-v67';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{
+ const url=new URL(e.request.url);
+ if(e.request.method!=='GET' || url.origin!==self.location.origin)return;
+ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match('./index.html'))));
+});
+
+self.addEventListener('message',e=>{if(e.data?.type==='NOTIFY')self.registration.showNotification(e.data.title||'Mis cosas',{body:e.data.body||'',tag:e.data.tag||'mis-cosas',icon:'./icon-192.png'});});
